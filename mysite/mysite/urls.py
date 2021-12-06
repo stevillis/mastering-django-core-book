84559d301f0053362ba22775025ac48d13129cb1
2019201410840742 <stevillis@hotmail.com>
2021-12-06 17:08:09 -0400

    Create dynamic content view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import hello
+from .views import hello, current_datetime
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello),
+    path('time/', current_datetime),
 ]
