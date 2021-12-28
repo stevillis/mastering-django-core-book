18566129ee6aba358ac646e7c08b76d9bfbc6d75
2019201410840742 <stevillis@hotmail.com>
2021-12-28 16:57:13 -0400

    Add HTML tags to the HttpResponse

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -19,5 +19,5 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "In %s hour(s), it will be %s." % (offset, dt)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
     return HttpResponse(html)
